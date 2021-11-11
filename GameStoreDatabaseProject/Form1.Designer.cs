d2c72aa8727f13a453551bd0075969c14668539b
Justin Koegeboehn <jwbean18@gmail.com>
2021-11-11 13:19:35 -0600

    Add project files.

---
+++ GameStoreDatabaseProject/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace GameStoreDatabaseProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserList = new System.Windows.Forms.ComboBox();
+            this.GameBox = new System.Windows.Forms.ListBox();
+            this.GameDetails = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RatingTrackBar = new System.Windows.Forms.TrackBar();
+            this.RatingScore = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ReviewDescription = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.RatingTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UserList
+            // 
+            this.UserList.FormattingEnabled = true;
+            this.UserList.Location = new System.Drawing.Point(667, 12);
+            this.UserList.Name = "UserList";
+            this.UserList.Size = new System.Drawing.Size(121, 21);
+            this.UserList.TabIndex = 0;
+            // 
+            // GameBox
+            // 
+            this.GameBox.FormattingEnabled = true;
+            this.GameBox.Location = new System.Drawing.Point(58, 128);
+            this.GameBox.Name = "GameBox";
+            this.GameBox.Size = new System.Drawing.Size(181, 264);
+            this.GameBox.TabIndex = 1;
+            // 
+            // GameDetails
+            // 
+            this.GameDetails.AutoSize = true;
+            this.GameDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameDetails.Location = new System.Drawing.Point(272, 128);
+            this.GameDetails.Name = "GameDetails";
+            this.GameDetails.Size = new System.Drawing.Size(86, 13);
+            this.GameDetails.TabIndex = 2;
+            this.GameDetails.Text = "Game Details:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Price:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Developer: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Release Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(272, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Five Star Rating:";
+            // 
+            // RatingTrackBar
+            // 
+            this.RatingTrackBar.LargeChange = 10;
+            this.RatingTrackBar.Location = new System.Drawing.Point(601, 195);
+            this.RatingTrackBar.Maximum = 50;
+            this.RatingTrackBar.Name = "RatingTrackBar";
+            this.RatingTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.RatingTrackBar.TabIndex = 7;
+            this.RatingTrackBar.Value = 25;
+            this.RatingTrackBar.Scroll += new System.EventHandler(this.RatingTrackBar_Scroll);
+            // 
+            // RatingScore
+            // 
+            this.RatingScore.Location = new System.Drawing.Point(667, 169);
+            this.RatingScore.Name = "RatingScore";
+            this.RatingScore.Size = new System.Drawing.Size(38, 20);
+            this.RatingScore.TabIndex = 8;
+            this.RatingScore.Text = "2.5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(581, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Rating out of 5:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(584, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Create Review";
+            // 
+            // ReviewDescription
+            // 
+            this.ReviewDescription.Location = new System.Drawing.Point(582, 253);
+            this.ReviewDescription.Multiline = true;
+            this.ReviewDescription.Name = "ReviewDescription";
+            this.ReviewDescription.Size = new System.Drawing.Size(170, 20);
+            this.ReviewDescription.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(272, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Genre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ReviewDescription);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.RatingScore);
+            this.Controls.Add(this.RatingTrackBar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GameDetails);
+            this.Controls.Add(this.GameBox);
+            this.Controls.Add(this.UserList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.RatingTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox UserList;
+        private System.Windows.Forms.ListBox GameBox;
+        private System.Windows.Forms.Label GameDetails;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar RatingTrackBar;
+        private System.Windows.Forms.TextBox RatingScore;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox ReviewDescription;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ GameStoreDatabaseProject/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameStoreDatabaseProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<String> listOfUsers = new List<string> { "User1", "User2", "user3", "user4", "user5" };
+            foreach (String user in listOfUsers)
+            {
+                UserList.Items.Add(user);
+            }
+        }
+
+        private void RatingTrackBar_Scroll(object sender, System.EventArgs e)
+        {
+            // Display the trackbar value in the text box.
+            RatingScore.Text = "" + ((double)RatingTrackBar.Value / 10);
+        }
+    }
+}
